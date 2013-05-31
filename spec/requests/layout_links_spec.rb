61059e5c28d2641ef486cdff66339bc71051dcda
unknown <be323492@bez5544c.ww002.siemens.net>
2013-05-31 18:50:05 +0200

    CSS setup

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,23 @@
+﻿require 'spec_helper'
+
+describe "LayoutLinks" do
+	it "devrait trouver une page Accueil à '/'" do
+		get '/'
+		response.should have_selector('title', :content => "Acceuil")
+	end
+	
+	it "devrait trouver une page Contact à '/contact'" do
+		get '/contact'
+		response.should have_selector('title', :content => "Contact")
+	end
+	
+	it "devrait trouver une page A propos à '/about'" do
+		get '/about'
+		response.should have_selector('title', :content => "A Propos")
+	end
+	
+	it "devrait trouver une page Aide à '/help'" do
+		get '/help'
+		response.should have_selector('title', :content => "Aide")
+	end
+end
